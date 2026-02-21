971141528819b5cedf7ff9ee32582dca55569d11
Rouf -Myz <rouf08412@gmail.com>
2026-02-21 13:47:37 +0700

    update region default

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-west-2"
+  region     = "your region here"
   access_key = "your access_key here"
   secret_key = "your secret_key here"
   token = "your token here"
